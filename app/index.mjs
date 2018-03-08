c649ae473e1a00024c6799edcb008f91b264d12a
nullpoo <fmfmfmfm@gmail.com>
2018-03-09 02:55:24 +0900

    Same free schedules then do not notify

--- app/index.mjs
+++ app/index.mjs
@@ -1,11 +1,15 @@
 import { scheduleChecker } from './schedule_checker';
 import { notifyForIfttt, notifyForZapier } from './notifier';
 
+let notifiedSchedules = [];
+
 const mainProcess = async () => {
   const schedules = await scheduleChecker();
-  if (schedules.length > 0) {
+
+  if (schedules.length !== notifiedSchedules.length && schedules.length > 0) {
     await notifyForIfttt(schedules);
     await notifyForZapier(schedules);
+    notifiedSchedules = schedules;
   }
 };
 
